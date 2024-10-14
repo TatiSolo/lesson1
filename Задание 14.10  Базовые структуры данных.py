063fd296cb876509a4b30b6b181053a5b792f1d3
TatiSolo <aleksandrsolo1604@gmail.com>
2024-10-14 21:02:48 +0500

    Add files via upload

---
+++ Задание 14.10  Базовые структуры данных.py
@@ -0,0 +1,48 @@
+print(12+89-1)
+print(5)
+print(type(5),type('5'))
+print(5+5)
+print(5-5)
+print(5*5)
+print(5/5)
+print(5//5)
+print(13%2)
+print(5**5)
+print(type(2.0))
+print(type(5+5.0))
+print(type('"Hello,world!"'))
+print('"Heiio,'+' world!"')
+print('1'+'1')
+print(1+1)
+print(True,False)
+print(type(True),type(False))
+print(5>10)
+print(5<10)
+print(5<10,5>10)
+print(1,2,3,4,'hello, world!',True)
+print(5<=3)
+print(5>=3)
+print(5==5)
+print(5!=3)
+print(5==5 and 5<10 and  5<4)
+print(5!=5 or 5<10)
+print(int('5'))
+print(type(int('5')))
+
+#"1st program"
+print(9**0.5*5)
+#"2st program"
+print(99>98 and 1000!=1000.1)
+#"3st program"
+print(1234%1200+5678//100)
+#"4st program"
+print(int(13.42)==int(42.13*100%42)and int(42.13)==int(13.42*100%130))
+
+#"1st program"
+print(9**0.5*5)
+#"2st program"
+print(99.99>9.98 and 1000!=1000.1)
+#"3st program"
+print(2*2+2==2*(2+2))
+#"4st program"
+print(int(float('123.456')*10)%10)
